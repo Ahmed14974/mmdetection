495ad819af6060576d9e536f6db0993a416bafd0
Yuzu Ido <yuzu@scdt.stanford.edu>
2022-10-28 12:03:47 -0700

    Implemented DLO step E prune

--- dlo/dlo.py
+++ dlo/dlo.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np 
 from PIL import Image, ImageDraw
 import cv2 as cv 
+import itertools
 
 l_s = 10 # 10 pixels is their param. Can improve
 max_angle = 0.25 # radians
+rect_width = 3 #pixels
 
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
@@ -29,6 +31,7 @@ def angle_between(s, new_s):
 
 """
 Implemented DLO paper step D using pseudocode.
+Given contour, map chains of fixed-len line segments.
 """
 def traverseContour(cnt):
     print(cnt.shape) #ndarray shape (num_pts, 1, 2)
@@ -69,26 +72,153 @@ def traverseContour(cnt):
     print(action_types)
     return colln
 
+"""
+Get skeleton from white on black image.
+"""
+def skeleton(image):
+    thinned = thin(image, max_iter = 5).astype(np.uint8) #ndarray -> ndarray(bool)
+    print(np.count_nonzero(thinned))
+    thinned_proc = 255 * thinned
+    # Image.fromarray(thinned_proc).save("dlo_thin_max5.png")
+    print(thinned.shape)
+    return thinned 
+
+def draw_chain_collns(chain_collns, h, w):
+    vis = np.zeros((h, w, 3), np.uint8)
+    color = 0
+    for colln in chain_collns:
+        for chain in colln:
+            for segment in chain:
+                cv.line(vis, segment[0][0], segment[1][0], (color, 255 - color, 255), 1)
+            print(np.count_nonzero(vis))
+            color += 30
+    Image.fromarray(vis).save(f"dlo_segments_pruned.png")
+
+"""
+Given midpoints of opposite side of rectangle and fixed width, find corners.
+Returned in counterclockwise order.
+"""
+# https://stackoverflow.com/questions/71582441/draw-a-rectangle-using-points-using-mid-points-of-opposite-sides
+def get_corners(point1,point2,width):
+    m1 = (point1[1]-point2[1])/(point1[0]-point2[0])
+    m2 = -1/m1
+    cor_x = np.sqrt((width/2)**2 / (m2**2 + 1))
+    cor_y = np.sqrt((width/2)**2 / (m2**-2 + 1))
+    if m2 >= 0:
+        corner1 = (point1[0] + cor_x, point1[1] + cor_y)
+        corner2 = (point1[0] - cor_x, point1[1] - cor_y)
+        corner3 = (point2[0] - cor_x, point2[1] - cor_y)
+        corner4 = (point2[0] + cor_x, point2[1] + cor_y)
+    else:
+        corner1 = (point1[0] - cor_x, point1[1] + cor_y)
+        corner2 = (point1[0] + cor_x, point1[1] - cor_y)
+        corner3 = (point2[0] + cor_x, point2[1] - cor_y)
+        corner4 = (point2[0] - cor_x, point2[1] + cor_y)
+    return corner1, corner2, corner3, corner4
+
+"""
+https://stackoverflow.com/questions/10962379/how-to-check-intersection-between-2-rotated-rectangles
+Checks if the two polygons are intersecting.
+"""
+def do_polygons_intersect(a, b):
+    """
+ * Helper function to determine whether there is an intersection between the two polygons described
+ * by the lists of vertices. Uses the Separating Axis Theorem
+ *
+ * @param a an ndarray of connected points [[x_1, y_1], [x_2, y_2],...] that form a closed polygon
+ * @param b an ndarray of connected points [[x_1, y_1], [x_2, y_2],...] that form a closed polygon
+ * @return true if there is any intersection between the 2 polygons, false otherwise
+    """
+    polygons = [a, b]
+    minA, maxA, projected, i, i1, j, minB, maxB = None, None, None, None, None, None, None, None
+    for i in range(len(polygons)):
+        # for each polygon, look at each edge of the polygon, and determine if it separates
+        # the two shapes
+        polygon = polygons[i]
+        for i1 in range(len(polygon)):
+            # grab 2 vertices to create an edge
+            i2 = (i1 + 1) % len(polygon)
+            p1 = polygon[i1]
+            p2 = polygon[i2]
+            # find the line perpendicular to this edge
+            normal = { 'x': p2[1] - p1[1], 'y': p1[0] - p2[0] }
+            minA, maxA = None, None
+            # for each vertex in the first shape, project it onto the line perpendicular to the edge
+            # and keep track of the min and max of these values
+            for j in range(len(a)):
+                projected = normal['x'] * a[j][0] + normal['y'] * a[j][1]
+                if (minA is None) or (projected < minA): 
+                    minA = projected
+                if (maxA is None) or (projected > maxA):
+                    maxA = projected
+            # for each vertex in the second shape, project it onto the line perpendicular to the edge
+            # and keep track of the min and max of these values
+            minB, maxB = None, None
+            for j in range(len(b)): 
+                projected = normal['x'] * b[j][0] + normal['y'] * b[j][1]
+                if (minB is None) or (projected < minB):
+                    minB = projected
+                if (maxB is None) or (projected > maxB):
+                    maxB = projected
+            # if there is no overlap between the projects, the edge we are looking at separates the two
+            # polygons, and we know there is no overlap
+            if (maxA < minB) or (maxB < minA):
+                return False
+    return True
+
+
+"""
+Given 2 fixed-len segments, make their respective rectangles
+and return whether they overlap.
+"""
+def overlap_segs(seg1, seg2):
+    rect1 = get_corners(seg1[0][0], seg1[1][0], rect_width)
+    rect2 = get_corners(seg2[0][0], seg2[1][0], rect_width)
+    return do_polygons_intersect(rect1, rect2)
+
+"""
+Step E of DLO. Given collection of possibly overlapping DOO chains,
+prune overlapping segments.
+"""
+def prune(chain_colln):
+    print(chain_colln)
+    num_intersect = 0
+    for i in range(len(chain_colln)):
+        for j in range(i + 1, len(chain_colln)):
+            shorter = None
+            longer = None
+            if len(chain_colln[i]) < len(chain_colln[j]):
+                shorter = i 
+                longer = j
+            else:
+                shorter = j
+                longer = i
+            keep = [True for _ in range(len(chain_colln[shorter]))]
+            for k in range(len(chain_colln[shorter])):
+                for seg2 in chain_colln[longer]:
+                    if overlap_segs(chain_colln[shorter][k], seg2):
+                        num_intersect += 1
+                        keep[k] = False
+            chain_colln[shorter] = list(itertools.compress(chain_colln[shorter], keep))
+    print(f"{num_intersect} intersections")
+    print(chain_colln)
+    return chain_colln
+            
+
+
 def dlo():
     # read data. Currently dataset masks but probably want 
     # model output masks eventually
     df = pd.read_csv('/deep/group/aicc-bootcamp/cloud-pollution/data/'\
                     'combined_v3_typed_new_composite/COCO_format_cropped/train.csv')
     df = df[df['contains_shiptrack']]
-    image = df.iloc[0]['mask']   # loop over rows
+    image = df.iloc[0]['mask']   # loop over rows eventually
     image = np.load(image)      
     print(np.count_nonzero(image))
     image_proc = np.abs(image.astype(int)).astype(np.uint8)
     # Image.fromarray(image_proc).save("dlo_orig.png")
 
     # Skeletonize
-    def skeleton(image):
-        thinned = thin(image, max_iter = 5).astype(np.uint8) #ndarray -> ndarray(bool)
-        print(np.count_nonzero(thinned))
-        thinned_proc = 255 * thinned
-        # Image.fromarray(thinned_proc).save("dlo_thin_max5.png")
-        print(thinned.shape)
-        return thinned 
     thinned = skeleton(image)
 
     # Find contours
@@ -104,17 +234,10 @@ def dlo():
     print(np.count_nonzero(vis))
     Image.fromarray(vis).save("dlo_contour_max5.png")
     
-    #fit doo segments 
+    #fit and prune doo segments 
     chain_collns = [traverseContour(contour) for contour in contours0]
-    vis = np.zeros((h, w, 3), np.uint8)
-    color = 0
-    for colln in chain_collns:
-        for chain in colln:
-            for segment in chain:
-                cv.line(vis, segment[0][0], segment[1][0], (color, 255 - color, 255), 1)
-            print(np.count_nonzero(vis))
-        color += 30
-    Image.fromarray(vis).save(f"dlo_segments_all.png")
-            
+    # draw_chain_collns(chain_collns, h, w)
+    pruned = [prune(chain_colln) for chain_colln in chain_collns]
+    draw_chain_collns(pruned, h, w)
 
 dlo()
